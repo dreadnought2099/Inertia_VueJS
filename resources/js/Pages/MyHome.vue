174bb70624c0b56b5e541045d11788fd17f0cd94
dreadnought2099 <magallanesraymart@gmail.com>
2025-01-21 16:14:09 +0800

    first commit

---
+++ resources/js/Pages/MyHome.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    props: {
+        films: Array,
+    },
+    data() {
+        return {
+            name: "RMAGALLANEZ",
+        };
+    },
+};
+</script>
+
+<template>
+    <h1 class="text-3xl">Welcome to Oasis!</h1>
+    <div class="card"></div>
+
+    <div class="card-body">
+        <p class="text-lg">Hello my name is {{ name }}.</p>
+        <input type="text" v-model="name" />
+    </div>
+
+    <ul>
+        <li v-for="film in films" :key="film"></li>
+        {{
+            film
+        }}
+    </ul>
+</template>
