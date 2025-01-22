40e289540544b4fe9490435573f519b6c07d15b8
dreadnought2099 <magallanesraymart@gmail.com>
2025-01-22 15:53:48 +0800

    add text bold

--- resources/js/Pages/MyHome.vue
+++ resources/js/Pages/MyHome.vue
@@ -17,7 +17,7 @@ export default {
         <div class="card-body">
             <p class="text-lg">Hello my name is {{ name }}</p>
             <input type="text" v-model="name" />
-            <p class="text-lg">Four Favourite Films</p>
+            <p class="text-lg font-bold">Four Favourite Films</p>
         </div>
 
         <ul>
